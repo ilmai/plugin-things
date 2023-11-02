60ec030b20582754faf4378b745c6b0faa7d8f16
Jussi Viiri <ilmai@iki.fi>
2023-11-02 19:00:14 +0200

    Move commented out code to relevant place

--- nih_plug_slint/src/plugin_component_handle.rs
+++ nih_plug_slint/src/plugin_component_handle.rs
@@ -11,33 +11,3 @@ pub trait PluginComponentHandle {
     fn update_parameter(&self, id: &str, update_value: bool, update_modulation: bool);
     fn update_all_parameters(&self);
 }
-
-// fn update_parameter(&self, id: &str, update_value: bool, update_modulation: bool) {
-//     let context = self.context.borrow();
-//     let context = context.as_ref().unwrap();
-
-//     // if let Some(param_ptr) = context.param_map.get(id) {
-//     //     if let Ok(Value::Struct(mut plugin_parameter)) = context.component.get_global_property(&context.parameter_globals_name, &id) {
-//     //         let value = unsafe { param_ptr.unmodulated_normalized_value() };
-//     //         let modulation = unsafe { param_ptr.modulated_normalized_value() - value };
-
-//     //         if update_value {
-//     //             let display_value = unsafe { param_ptr.normalized_value_to_string(value, true) };
-
-//     //             plugin_parameter.set_field("value".into(), Value::Number(value as f64));
-//     //             plugin_parameter.set_field("display-value".into(), Value::String(display_value.into()));    
-//     //             plugin_parameter.set_field("modulation".into(), Value::Number(modulation as f64));
-//     //         } else if update_modulation {
-//     //             plugin_parameter.set_field("modulation".into(), Value::Number(modulation as f64));
-//     //         }
-
-//     //         context.component.set_global_property(&context.parameter_globals_name, id, Value::Struct(plugin_parameter)).unwrap();
-//     //     }
-//     // }
-// }
-
-// fn update_all_parameters(&self) {
-//     for id in self.ui_parameters.borrow().iter() {
-//         self.update_parameter(id, true, true);
-//     }
-// }
