e5237420682de89ec4d83b9ca3eaf5773832fa6c
Jussi Viiri <ilmai@iki.fi>
2023-11-10 14:32:18 +0200

    Slint file reorganization
    - Move knob.slint & slider.slint to demo directory
    - Rename components directory -> slint
    - Use new library feature to refer to parameter.slint

--- examples/demo/build.rs
+++ examples/demo/build.rs
@@ -1,8 +1,8 @@
 fn main() {
     let config = slint_build::CompilerConfiguration::new()
-        .with_include_paths(vec![
-            "../../nih_plug_slint/components/".into(),
-        ]);
+        .with_library_paths([
+            ("parameter".into(), "../../nih_plug_slint/slint/parameter.slint".into()),
+        ].into());
 
     slint_build::compile_with_config("main.slint", config).unwrap();
 }
