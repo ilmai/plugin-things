fe59bc172704afd9c5d1a3e76db07843ec98f513
Jussi Viiri <ilmai@iki.fi>
2023-11-14 22:57:29 +0200

    Re-export Window from plugin-canvas

--- nih_plug_slint/src/lib.rs
+++ nih_plug_slint/src/lib.rs
@@ -8,4 +8,4 @@ pub mod window_adapter;
 pub use plugin_canvas::dimensions::{LogicalPosition, LogicalSize};
 pub use plugin_canvas::drag_drop::{DropData, DropOperation};
 pub use plugin_canvas::event::{Event, EventResponse};
-pub use plugin_canvas::window::WindowAttributes;
+pub use plugin_canvas::window::{Window, WindowAttributes};
