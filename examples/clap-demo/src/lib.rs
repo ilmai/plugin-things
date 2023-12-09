9bccf583cb34bf1e640f3f1171702b2a26c7568f
Jussi Viiri <ilmai@iki.fi>
2023-12-09 23:17:41 +0200

    Add gain parameter to CLAP demo

--- examples/clap-demo/src/lib.rs
+++ examples/clap-demo/src/lib.rs
@@ -1,7 +1,9 @@
-use std::ffi::CStr;
+use core::fmt::Write;
+use std::{ffi::CStr, sync::Arc};
 
-use clack_extensions::audio_ports::{AudioPortInfoWriter, AudioPortInfoData, AudioPortFlags, AudioPortType};
-use clack_plugin::prelude::*;
+use clack_extensions::{audio_ports::{AudioPortInfoWriter, AudioPortInfoData, AudioPortFlags, AudioPortType}, params::{implementation::{ParamInfoWriter, ParamDisplayWriter}, info::{ParamInfoData, ParamInfoFlags}}};
+use clack_plugin::{prelude::*, utils::Cookie, events::{event_types::ParamValueEvent, Event}};
+use portable_atomic::{AtomicF64, Ordering};
 
 pub struct DemoPlugin {
 }
@@ -21,18 +23,59 @@ impl clack_plugin::plugin::Plugin for DemoPlugin {
     }
 
     fn declare_extensions(builder: &mut PluginExtensions<Self>, _shared: &Self::Shared<'_>) {
-        builder.register::<clack_extensions::audio_ports::PluginAudioPorts>();
+        builder
+            .register::<clack_extensions::audio_ports::PluginAudioPorts>()
+            .register::<clack_extensions::params::PluginParams>();
     }
 }
 
-pub struct DemoPluginAudioProcessor;
+pub struct DemoPluginParameters {
+    gain: AtomicF64,
+}
 
-impl<'a> clack_plugin::plugin::PluginAudioProcessor<'a, (), DemoPluginMainThread> for DemoPluginAudioProcessor {
-    fn activate(_host: HostAudioThreadHandle<'a>, _main_thread: &mut DemoPluginMainThread, _shared: &'a (), _audio_config: AudioConfiguration) -> Result<Self, PluginError> {
-        Ok(Self)
+impl Default for DemoPluginParameters {
+    fn default() -> Self {
+        Self {
+            gain: Default::default()
+        }
+    }
+}
+
+pub struct DemoPluginAudioProcessor {
+    parameters: Arc<DemoPluginParameters>,
+}
+
+impl DemoPluginAudioProcessor {
+    fn new(parameters: Arc<DemoPluginParameters>) -> Self {
+        Self {
+            parameters,
+        }
     }
 
-    fn process(&mut self, _process: Process, mut audio: Audio, _events: Events) -> Result<ProcessStatus, PluginError> {
+    fn process_event(&mut self, event: &UnknownEvent) {
+        match event.header().type_id() {
+            ParamValueEvent::TYPE_ID => {
+                let param_value_event = event.as_event::<ParamValueEvent>().unwrap();
+                assert_eq!(param_value_event.param_id(), 0);
+                self.parameters.gain.store(param_value_event.value(), Ordering::Relaxed);
+            },
+
+            _ => {},
+        }
+    }
+}
+
+
+impl<'a> clack_plugin::plugin::PluginAudioProcessor<'a, (), DemoPluginMainThread> for DemoPluginAudioProcessor {
+    fn activate(_host: HostAudioThreadHandle<'a>, main_thread: &mut DemoPluginMainThread, _shared: &'a (), _audio_config: AudioConfiguration) -> Result<Self, PluginError> {
+        Ok(Self::new(main_thread.parameters.clone()))
+    }
+
+    fn process(&mut self, _process: Process, mut audio: Audio, events: Events) -> Result<ProcessStatus, PluginError> {
+        for event in events.input {
+            self.process_event(event);
+        }
+
         for mut port_pair in &mut audio {
             let Some(channel_pairs) = port_pair.channels()?.into_f32() else { continue; };
 
@@ -52,11 +95,28 @@ impl<'a> clack_plugin::plugin::PluginAudioProcessor<'a, (), DemoPluginMainThread
     }
 }
 
-pub struct DemoPluginMainThread;
+impl clack_extensions::params::implementation::PluginAudioProcessorParams for DemoPluginAudioProcessor {
+    fn flush(
+        &mut self,
+        input_parameter_changes: &InputEvents,
+        _output_parameter_changes: &mut OutputEvents,
+    )
+    {
+        for event in input_parameter_changes {
+            self.process_event(event);
+        }
+    }
+}
+
+pub struct DemoPluginMainThread {
+    parameters: Arc<DemoPluginParameters>,
+}
 
 impl<'a> clack_plugin::plugin::PluginMainThread<'a, ()> for DemoPluginMainThread {
     fn new(_host: HostMainThreadHandle<'a>, _shared: &'a ()) -> Result<Self, PluginError> {
-        Ok(Self)
+        Ok(Self {
+            parameters: Default::default(),
+        })
     }
 }
 
@@ -76,6 +136,57 @@ impl clack_extensions::audio_ports::PluginAudioPortsImpl for DemoPluginMainThrea
             port_type: Some(AudioPortType::STEREO),
             in_place_pair: Some(0),
         });
+    }
+}
+
+impl clack_extensions::params::implementation::PluginMainThreadParams for DemoPluginMainThread {
+    fn count(&self) -> u32 {
+        1
+    }
+
+    fn get_info(&self, param_index: u32, info: &mut ParamInfoWriter) {
+        assert_eq!(param_index, 0);
+        info.set(&ParamInfoData {
+            id: 0,
+            flags: ParamInfoFlags::IS_AUTOMATABLE |
+                ParamInfoFlags::IS_MODULATABLE |
+                ParamInfoFlags::REQUIRES_PROCESS,
+            cookie: Cookie::default(),
+            name: "Gain",
+            module: "",
+            min_value: -80.0,
+            max_value: 0.0,
+            default_value: 0.0,
+        });
+    }
+
+    fn get_value(&self, param_id: u32) -> Option<f64> {
+        assert_eq!(param_id, 0);
+        Some(self.parameters.gain.load(Ordering::Relaxed))
+    }
+
+    fn value_to_text(
+        &self,
+        param_id: u32,
+        value: f64,
+        writer: &mut ParamDisplayWriter,
+    ) -> core::fmt::Result {
+        assert_eq!(param_id, 0);
+        write!(writer, "{value:.1} dB")
+    }
+
+    fn text_to_value(&self, param_id: u32, text: &str) -> Option<f64> {
+        assert_eq!(param_id, 0);
+        // TODO
+        None
+    }
+
+    fn flush(
+        &mut self,
+        input_parameter_changes: &InputEvents,
+        output_parameter_changes: &mut OutputEvents,
+    )
+    {
     }
 }
 
