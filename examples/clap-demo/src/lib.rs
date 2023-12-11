209bd2dd52b98d168cd94803725a3db4387a2eba
Jussi Viiri <ilmai@iki.fi>
2023-12-11 23:53:14 +0200

    Start of implementing CLAP GUI extension

--- examples/clap-demo/src/lib.rs
+++ examples/clap-demo/src/lib.rs
@@ -1,8 +1,9 @@
 use core::fmt::Write;
 use std::{ffi::CStr, sync::Arc, iter::zip};
 
-use clack_extensions::{audio_ports::{AudioPortInfoWriter, AudioPortInfoData, AudioPortFlags, AudioPortType}, params::{implementation::{ParamInfoWriter, ParamDisplayWriter}, info::{ParamInfoData, ParamInfoFlags}}};
+use clack_extensions::{audio_ports::{AudioPortInfoWriter, AudioPortInfoData, AudioPortFlags, AudioPortType}, params::{implementation::{ParamInfoWriter, ParamDisplayWriter}, info::{ParamInfoData, ParamInfoFlags}}, gui::{GuiApiType, GuiConfiguration, GuiResizeHints, GuiSize}};
 use clack_plugin::{prelude::*, utils::Cookie, events::{event_types::ParamValueEvent, Event}};
+use plugin_canvas::{window::WindowAttributes, LogicalSize, event::EventResponse};
 use portable_atomic::{AtomicF64, Ordering};
 
 pub struct DemoPlugin {
@@ -25,6 +26,7 @@ impl clack_plugin::plugin::Plugin for DemoPlugin {
     fn declare_extensions(builder: &mut PluginExtensions<Self>, _shared: &Self::Shared<'_>) {
         builder
             .register::<clack_extensions::audio_ports::PluginAudioPorts>()
+            .register::<clack_extensions::gui::PluginGui>()
             .register::<clack_extensions::params::PluginParams>();
     }
 }
@@ -114,12 +116,32 @@ impl clack_extensions::params::implementation::PluginAudioProcessorParams for De
 
 pub struct DemoPluginMainThread {
     parameters: Arc<DemoPluginParameters>,
+    window_size: LogicalSize,
+    os_scale: f64,
+    user_scale: f64,
+}
+
+impl DemoPluginMainThread {
+    fn process_event(&mut self, event: &UnknownEvent) {
+        match event.header().type_id() {
+            ParamValueEvent::TYPE_ID => {
+                let param_value_event = event.as_event::<ParamValueEvent>().unwrap();
+                assert_eq!(param_value_event.param_id(), 0);
+                self.parameters.gain.store(param_value_event.value(), Ordering::Relaxed);
+            },
+
+            _ => {},
+        }
+    }
 }
 
 impl<'a> clack_plugin::plugin::PluginMainThread<'a, ()> for DemoPluginMainThread {
     fn new(_host: HostMainThreadHandle<'a>, _shared: &'a ()) -> Result<Self, PluginError> {
         Ok(Self {
             parameters: Default::default(),
+            window_size: LogicalSize::new(0.0, 0.0),
+            os_scale: 1.0,
+            user_scale: 1.0,
         })
     }
 }
@@ -140,6 +162,98 @@ impl clack_extensions::audio_ports::PluginAudioPortsImpl for DemoPluginMainThrea
             port_type: Some(AudioPortType::STEREO),
             in_place_pair: Some(0),
         });
+    }
+}
+
+impl clack_extensions::gui::PluginGuiImpl for DemoPluginMainThread {
+    fn is_api_supported(&self, configuration: clack_extensions::gui::GuiConfiguration) -> bool {
+        if configuration.is_floating {
+            return false;
+        }
+
+        Some(configuration.api_type) == GuiApiType::default_for_current_platform()
+    }
+
+    fn get_preferred_api(&self) -> Option<clack_extensions::gui::GuiConfiguration> {
+        GuiApiType::default_for_current_platform()
+            .and_then(|api_type| {
+                Some(GuiConfiguration {
+                    api_type,
+                    is_floating: false,
+                })            
+            })
+    }
+
+    fn create(&mut self, configuration: clack_extensions::gui::GuiConfiguration) -> Result<(), clack_extensions::gui::GuiError> {
+        if !self.is_api_supported(configuration) {
+            return Err(clack_extensions::gui::GuiError::CreateError);
+        }
+
+        // We can't create anything without a parent, so just return
+        Ok(())
+    }
+
+    fn destroy(&mut self) {
+        todo!()
+    }
+
+    fn set_scale(&mut self, scale: f64) -> Result<(), clack_extensions::gui::GuiError> {
+        #[cfg(target_os = "macos")]
+        return Err(GuiError::SetScaleError);
+
+        self.os_scale = scale;
+
+        Ok(())
+    }
+
+    fn get_size(&mut self) -> Option<clack_extensions::gui::GuiSize> {
+        Some(GuiSize {
+            width: self.window_size.width.round() as u32,
+            height: self.window_size.height.round() as u32,
+        })
+    }
+
+    fn can_resize(&self) -> bool {
+        // TODO
+        false
+    }
+
+    /// Provide hints on the resize-ability of the GUI
+    fn get_resize_hints(&self) -> Option<GuiResizeHints> {
+        // TODO
+        None
+    }
+
+    fn set_size(&mut self, size: clack_extensions::gui::GuiSize) -> Result<(), clack_extensions::gui::GuiError> {
+        self.window_size.width = size.width as f64;
+        self.window_size.height = size.height as f64;
+
+        Ok(())
+    }
+
+    fn set_parent(&mut self, window: clack_extensions::gui::Window) -> Result<(), clack_extensions::gui::GuiError> {
+        plugin_canvas::Window::open(
+            window,
+            WindowAttributes::new(self.window_size.clone(), self.user_scale),
+            self.os_scale,
+            Box::new(|event| { EventResponse::Ignored }),
+            Box::new(|window| {}),
+        ).map_err(|_| clack_extensions::gui::GuiError::SetParentError)?;
+
+        Ok(())
+    }
+
+    fn set_transient(&mut self, _window: clack_extensions::gui::Window) -> Result<(), clack_extensions::gui::GuiError> {
+        // Only used for floating windows which we don't support
+        Ok(())
+    }
+
+    fn show(&mut self) -> Result<(), clack_extensions::gui::GuiError> {
+        todo!()
+    }
+
+    fn hide(&mut self) -> Result<(), clack_extensions::gui::GuiError> {
+        todo!()
     }
 }
 
@@ -186,10 +300,13 @@ impl clack_extensions::params::implementation::PluginMainThreadParams for DemoPl
 
     fn flush(
         &mut self,
-        _input_parameter_changes: &InputEvents,
+        input_parameter_changes: &InputEvents,
         _output_parameter_changes: &mut OutputEvents,
     )
     {
+        for event in input_parameter_changes {
+            self.process_event(event);
+        }
     }
 }
 
