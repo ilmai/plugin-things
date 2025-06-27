4a888ce2e11fa86a56f8bda2b3c1e4ee7d691810
Jussi Viiri <ilmai@iki.fi>
2025-06-27 08:48:10 +0300

    Implement DoubleEndedIterator for FramesIterator(Mut)

--- plinth-core/src/signals/frames_iterator.rs
+++ plinth-core/src/signals/frames_iterator.rs
@@ -4,14 +4,18 @@ use super::{signal::{Signal, SignalMut}, signal_frame::{SignalFrame, SignalFrame
 
 pub struct FramesIterator<'signal, S: Signal + ?Sized> {
     signal: &'signal S,
-    frame_index: usize,
+    frame_index_front: usize,
+    frame_index_back: usize,
 }
 
 impl<S: Signal> FramesIterator<'_, S> {
     pub fn new(signal: &S) -> FramesIterator<'_, S> {
+        let frame_index_back = if signal.is_empty() { 0 } else { signal.len() - 1 };
+
         FramesIterator {
             signal,
-            frame_index: 0,
+            frame_index_front: 0,
+            frame_index_back,
         }
     }
 }
@@ -20,12 +24,25 @@ impl<'signal, S: Signal> Iterator for FramesIterator<'signal, S> {
     type Item = SignalFrame<'signal, S>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.frame_index >= self.signal.len() {
+        if self.frame_index_front >= self.frame_index_back {
             return None;
         }
 
-        let result = self.signal.frame(self.frame_index);
-        self.frame_index += 1;
+        let result = self.signal.frame(self.frame_index_front);
+        self.frame_index_front += 1;
+
+        Some(result)
+    }
+}
+
+impl<'signal, S: Signal> DoubleEndedIterator for FramesIterator<'signal, S> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.frame_index_front >= self.frame_index_back {
+            return None;
+        }
+
+        let result = self.signal.frame(self.frame_index_back);
+        self.frame_index_back -= 1;
 
         Some(result)
     }
@@ -33,14 +50,18 @@ impl<'signal, S: Signal> Iterator for FramesIterator<'signal, S> {
 
 pub struct FramesIteratorMut<'signal, S: SignalMut + ?Sized> {
     signal: &'signal mut S,
-    frame_index: usize,
+    frame_index_front: usize,
+    frame_index_back: usize,
 }
 
 impl<S: SignalMut> FramesIteratorMut<'_, S> {
     pub fn new(signal: &mut S) -> FramesIteratorMut<'_, S> {
+        let frame_index_back = if signal.is_empty() { 0 } else { signal.len() - 1 };
+
         FramesIteratorMut {
             signal,
-            frame_index: 0,
+            frame_index_front: 0,
+            frame_index_back,
         }
     }
 }
@@ -49,12 +70,27 @@ impl<'signal, S: SignalMut> Iterator for FramesIteratorMut<'signal, S> {
     type Item = SignalFrameMut<'signal, S>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.frame_index >= self.signal.len() {
+        if self.frame_index_front >= self.frame_index_back {
             return None;
         }
 
-        let result = self.signal.frame_mut(self.frame_index);
-        self.frame_index += 1;
+        let result = self.signal.frame_mut(self.frame_index_front);
+        self.frame_index_front += 1;
+
+        // Re-borrow to the correct lifetime, which is safe since self.signal has the same lifetime
+        let result = unsafe { transmute::<SignalFrameMut<'_, S>, SignalFrameMut<'_, S>>(result) };
+        Some(result)
+    }
+}
+
+impl<'signal, S: SignalMut> DoubleEndedIterator for FramesIteratorMut<'signal, S> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.frame_index_front >= self.frame_index_back {
+            return None;
+        }
+
+        let result = self.signal.frame_mut(self.frame_index_back);
+        self.frame_index_back -= 1;
 
         // Re-borrow to the correct lifetime, which is safe since self.signal has the same lifetime
         let result = unsafe { transmute::<SignalFrameMut<'_, S>, SignalFrameMut<'_, S>>(result) };
